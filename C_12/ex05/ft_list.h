40d451f0eb2f05647f06d427aee3f298ad064418
Kangkai Ye <yek@MacBook-Pro.local>
2020-09-13 23:39:33 +0200

    list chaine jusqu a ex05

--- C_12/ex05/ft_list.h
+++ C_12/ex05/ft_list.h
@@ -1,37 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_create_elem.c                                   :+:      :+:    :+:   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kaye <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/27 14:28:16 by kaye              #+#    #+#             */
-/*   Updated: 2020/08/27 16:41:19 by kaye             ###   ########.fr       */
+/*   Created: 2020/08/27 14:28:32 by kaye              #+#    #+#             */
+/*   Updated: 2020/08/27 16:41:56 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include "ft_list.h"
-#include <stdio.h>
+#ifndef FT_LIST_H
+# define FT_LIST_H
 
-t_list		*ft_create_elem(void *data)
+typedef struct		s_list
 {
-	t_list *list;
-	
-	if (!(list = malloc(sizeof(t_list))))
-		return (NULL);
-	list->data = data;
-	list->next = NULL;
+	void			*data;
+	struct s_list	*next;
+}					t_list;
 
-	return (list);
-}
-
-int main()
-{
-	t_list *list;
-
-	list = ft_create_elem("element1");
-	if (list) // Verifier bien si les elements sont bien crees
-		printf("%s\n", list->data);
-	return (0);
-}
+#endif
